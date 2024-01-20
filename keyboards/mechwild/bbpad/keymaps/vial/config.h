cd0478331dcacab45534e0821ab849f8f7724d59
honorless <86894501+lesshonor@users.noreply.github.com>
2024-01-20 15:23:52 -0500

    feat: vial keymap

---
+++ keyboards/mechwild/bbpad/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+// Copyright 2023 Vial Contributors (@vial-qmk)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE6, 0xA0, 0x0C, 0x01, 0x7A, 0xFC, 0x67, 0xB6}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 3}
