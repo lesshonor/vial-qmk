e2a85b8493bd166982b8f4e1b3d9468c889eb886
honorless <86894501+lesshonor@users.noreply.github.com>
2024-01-29 21:38:01 -0500

    feat: vial keymap

---
+++ keyboards/mechwild/bb40/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+// Copyright 2024 Vial Contributors (@vial-kb)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5A, 0xB4, 0x5D, 0x11, 0x2C, 0x4B, 0x6A, 0x2B}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
