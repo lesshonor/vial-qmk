ed3b3b6af48e7c823629ee3a3a41ce187e6a4d68
honorless <86894501+lesshonor@users.noreply.github.com>
2024-02-28 21:37:22 -0500

    feat: Vial keymap for tkc/candybar/lefty

---
+++ keyboards/tkc/candybar/lefty/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDF, 0xA4, 0x3A, 0xAA, 0x9D, 0xD5, 0xF9, 0xF7}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
