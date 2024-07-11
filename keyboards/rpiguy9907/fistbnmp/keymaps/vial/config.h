b257b44d90e3e2e8b40c5e7a529644c65fc655a6
honorless <86894501+lesshonor@users.noreply.github.com>
2024-07-11 17:23:08 -0400

    feat(keyboard): fistbnmp vial keymap

---
+++ keyboards/rpiguy9907/fistbnmp/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5A, 0x53, 0x42, 0x61, 0xFA, 0x42, 0x3C, 0xC0}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 8 }
