f97264885008e1a39835eae52388abe013b9a69b
honorless <86894501+lesshonor@users.noreply.github.com>
2024-12-05 22:04:10 -0500

    feat: vial keymap

---
+++ keyboards/wilba_tech/wt45_h1/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2024 The Vial-KB Community (@vial-kb)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF0, 0x33, 0xD8, 0x03, 0x7C, 0x8D, 0xAC, 0xE0}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
